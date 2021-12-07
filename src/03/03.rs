77653a0d2b87b238e8b0fdb550007f4114a6591c
solidsniper <bauer__alex@hotmail.com>
2021-12-07 12:59:55 +0100

    hacky part 3

--- src/03/03.rs
+++ src/03/03.rs
@@ -1,7 +1,29 @@
 use itertools::Itertools;
+static INPUT: &str = include_str!("input.txt");
 
-static INPUT: &str = include_str!("/home/alex/Documents/aoc/01/aoc01/src/03/input.txt");
-
-fn main(){
-    println!("{:?}", INPUT);
+fn main() {
+  let words: Vec<&str> = INPUT.split('\n').collect();
+  let mut x = words.len() / 2;
+  let mut t: u32 = x.try_into().unwrap();
+  let mut xs: [u32; 12] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for pos in words.iter() {
+    for (i, o) in pos.chars().enumerate() {
+      xs[i] += o.to_digit(10).unwrap();
+      // println!("{}", x)
+    }
+  }
+  let mut bytestring: String = "".to_owned();
+  xs.iter_mut().for_each(|x| {
+    if x > &mut t {
+      *x = 1;
+      bytestring.push_str("1");
+    } else {
+      *x = 0;
+      bytestring.push_str("0");
+    }
+  });
+  println!("{:?}", xs);
+  let mut t: u32 = 0b000100011100;
+  let mut y: u32 = 0b111011100011;
+  println!("{},{},{}", t,y,t*y);
 }
